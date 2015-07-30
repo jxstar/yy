a72b6f63ecbdfe1913b0fd4330d85fc6aa32766b
jxstar <13911191965@139.com>
2015-07-30 22:12:19 +0800

    Signed-off-by: jxstar <13911191965@139.com>

--- yy20150726.R
+++ yy20150726.R
@@ -25,74 +25,6 @@ par(mfrow=c(1,1),mar=c(4,4,0,0))
 mycolor=rainbow(20)
 
 
-# about the space
-gc()
-
-#----------------------------------------------------------------------------------------------
-#1# original data
-if (F){
-  conn <- dbConnect(MySQL(), dbname = "thdata", username="thdata_user", password="gbgj53GD2s2gy64wRT",host="121.43.197.34",port=3306)
-  yydb201545 = dbGetQuery(conn,"select kid,sid,liveuid,users,typename,date from simple_online_duowan")
-  dbDisconnect(conn)
-  
-  
-  conn <- dbConnect(MySQL(), dbname = "yy", username="root", password="123456",host="127.0.0.1",port=3306)
-  #yydb20150102 = dbGetQuery(conn,"select kid,sid,liveuid,users,typename,date from yy20150102")
-  yydb20150203 = dbGetQuery(conn,"select kid,sid,liveuid,users,typename,date from yy20150203")
-  yydb201504 = dbGetQuery(conn,"select kid,sid,liveuid,users,typename,date from yy201504")
-  dbDisconnect(conn)
-  
-  #class(yydb20150102$date) = c('POSIXt','POSIXct')
-  #yydb20150102$mon=as.integer(format(as.Date(yydb20150102$date),"%Y%m"))
-  
-  class(yydb20150203$date) = c('POSIXt','POSIXct')
-  yydb20150203$mon=as.integer(format(as.Date(yydb20150203$date),"%Y%m"))
-  
-  class(yydb201504$date) = c('POSIXt','POSIXct')
-  yydb201504$mon=as.integer(format(as.Date(yydb201504$date),"%Y%m"))
-  
-  class(yydb201545$date) = c('POSIXt','POSIXct')
-  yydb201545$mon=as.integer(format(as.Date(yydb201545$date),"%Y%m"))
-  
-  
-  #yy=rbind(yydb20150102,yydb20150203,yydb201504)
-  yy=rbind(yydb20150203,yydb201504)
-  
-  yy=rbind(yy,yydb201545)  
-  save(file="./yy.Rdata",yy)
-  
-  yy201502=filter(yy,mon==201502)
-  yy201503=filter(yy,mon==201503)
-  yy201504=filter(yy,mon==201504)
-  yy201505=filter(yy,mon==201505)
-  #yy201502[seq(1,nrow(yy201502),10000),"date"]
-  save(file="yy201502.Rdata",yy201502)
-  save(file="yy201503.Rdata",yy201503)
-  save(file="yy201504.Rdata",yy201504)
-  save(file="yy201505.Rdata",yy201505)
-  
-  
-  conn <- dbConnect(MySQL(), dbname = "yy", username="root", password="123456",host="127.0.0.1",port=3306)
-  
-#20141001 20141101
-  (bunixstamp=as.numeric(as.POSIXct("2014-10-01")))
-  (eunixstamp=as.numeric(as.POSIXct("2014-11-01")))
-
-
-yy201410 = dbGetQuery(conn,'select kid,sid,liveuid,users,typename,date from duowan_yy where (date<UNIX_TIMESTAMP("2014-12-26") )&& (date>=UNIX_TIMESTAMP("2014-12-25"))')
-yy201410 = dbGetQuery(conn,"select kid,sid,liveuid,users,typename,date from duowan_yy where date>=1412092800 && date<1417363200")
-yy201410 = dbGetQuery(conn,"select kid,sid,liveuid,users,typename,date from duowan_yy")
-
-class(yy201410$date) = c('POSIXt','POSIXct')
-yy201410$mon=as.integer(format(as.Date(yy201410$date),"%Y%m"))
-head(yy201410)  
-tail(yy201410)  
-dbDisconnect(conn)
-  
-  
-}
-
-
 if (F){
 
 bdate="2015-04-01"
@@ -107,7 +39,7 @@ dbDisconnect(conn)
 class(yy$date) = c('POSIXt','POSIXct')
 yy$mon=as.integer(format(as.Date(yy$date),"%Y%m"))
 
-save(file=paste(bdate,edate,"yy.Rdate"),yy)
+save(file=paste(bdate,edate,"yy.Rdata"),yy)
 
 }
 
@@ -116,7 +48,7 @@ save(file=paste(bdate,edate,"yy.Rdate"),yy)
 #----------------------------------------------------------------------------------------------
 #2# load yy
 #gc()
-#load("./yy.Rdata")
+load("./2015-04-01 2015-07-31 yy.Rdata")
 yy=tbl_df(yy)
 
 #----------------------------------------------------------------------------------------------
@@ -151,7 +83,14 @@ for(monthnow in monthlist){
   mdnew[!(mdnew$liveuid %in% mdpre$liveuid),"uidnew"]=1
   mdpre[!(mdpre$liveuid %in% mdnew$liveuid),"uiddel"]=1
   
+  
+  
+  
+  
+  
   #-----------------------------whole without typename classify----------------------------
+  #==========sid===========================================================================
+  
   sidnewtot=group_by(mdnew,sid)
   sidnewadd=group_by(filter(mdnew,sidnew==1),sid)
   sidpredel=group_by(filter(mdpre,siddel==1),sid)
@@ -205,14 +144,16 @@ for(monthnow in monthlist){
   resid[3,]=data.frame(monthnow,as.character("delete"),length(unique(sidpredel$sid)),sum(smrdelsid$meanuser), length(unique(uidpredel$liveuid)), NA, NA, NA,NA,NA, NA, NA, NA,stringsAsFactors =F)
   resid=transform(resid,psmu10=smu10/sumuser,psmu5=smu5/sumuser,psmu1=smu1/sumuser,psmu0=smu0/sumuser)
   
-  write.csv(file=paste(monthnow,"sid user distribution specification.csv"),top_n(arrange(smrtotsid,desc(meanuser)),resid[1,"nmu10"]+resid[1,"nmu5"]+resid[1,"nmu1"],meanuser))
-#  write.csv(file=paste(monthnow,"sid user number  summary.csv"),resid)
+  #write.csv(file=paste(monthnow,"sid user distribution specification.csv"),top_n(arrange(smrt otsid,desc(meanuser)),resid[1,"nmu10"]+resid[1,"nmu5"]+resid[1,"nmu1"],meanuser))
+  #write.csv(file=paste(monthnow,"sid user number  summary.csv"),resid)
+  write.xlsx(x=resid,file=paste(monthnow,"yy data.xlsx"),sheetName = "SidSummary",append = T,showNA = F)
+  
 
   resid.list[[monthnow-monthlist[1]+1]]=resid
   names(resid.list)[monthnow-monthlist[1]+1]=as.character(monthnow)
   resid.list
   
-  #========================================================================================
+  #=========uid============================================================================
   
   reuid=data.frame(month=NA,project=NA,uniquesid=NA, sumuser=NA,uniqueuid=NA,nmu10=NA,smu10=NA,nmu5=NA,smu5=NA,nmu1=NA,smu1=NA,nmu0=NA,smu0=NA,
                    stringsAsFactors =F)
@@ -250,18 +191,27 @@ for(monthnow in monthlist){
   reuid[3,]=data.frame(monthnow,as.character("delete"),length(unique(sidpredel$sid)),sum(smrdelsid$meanuser), length(unique(uidpredel$liveuid)), NA, NA, NA,NA,NA, NA, NA, NA,stringsAsFactors =F)
   reuid=transform(reuid,psmu10=smu10/sumuser,psmu5=smu5/sumuser,psmu1=smu1/sumuser,psmu0=smu0/sumuser)
   
-  write.csv(file=paste(monthnow,"uid user distribution specification.csv"),top_n(arrange(smrtotuid,desc(meanuser)),reuid[1,"nmu10"]+reuid[1,"nmu5"]+reuid[1,"nmu1"],meanuser))
-#  write.csv(file=paste(monthnow,"uid user number summary.csv"),reuid)
+#  write.csv(file=paste(monthnow,"uid user distribution specification.csv"),top_n(arrange(smrtotuid,desc(meanuser)),reuid[1,"nmu10"]+reuid[1,"nmu5"]+reuid[1,"nmu1"],meanuser))
+  #write.csv(file=paste(monthnow,"uid user number summary.csv"),reuid)
+  write.xlsx(x=reuid,file=paste(monthnow,"yy data.xlsx"),sheetName = "UidSummary",append = T,showNA = F)
   
   reuid.list[[monthnow-monthlist[1]+1]]=reuid
   names(reuid.list)[monthnow-monthlist[1]+1]=as.character(monthnow)
   reuid.list
 
   
+  
+  
+  
+  
+  
+  
+  
   #-----------------------------by type statistic of the total date-----------------------------
   
   type.range=names(table(mdnew$typename))
-  type=c("entertain","fin","game","love","ting")
+  #type=c("entertain","fin","game","love","ting")
+  type=unique(mdnew$typename)
   resbytype=as.data.frame(matrix(NA,9,3+length(type)))
   names(resbytype)=c("month","project","valuename",type)
   resbytype$month=monthnow
@@ -278,11 +228,6 @@ for(monthnow in monthlist){
     sidnewadd=group_by(filter(mdnew,typename==tname & sidnew==1),sid)
     sidpredel=group_by(filter(mdpre,typename==tname & siddel==1),sid)
     
-    uidnewtot=group_by(filter(mdnew,typename==tname),liveuid)
-    uidnewadd=group_by(filter(mdnew,typename==tname & uidnew==1),liveuid)
-    uidpredel=group_by(filter(mdpre,typename==tname & uiddel==1),liveuid)
-    #length(group_size(gsidnew))
-    
     smrtotsid=summarise(sidnewtot,nsid=n(),nuser=sum(users),meanuser=mean(users),maxuser=max(users),minuser=min(users))
     smraddsid=summarise(sidnewadd,nsid=n(),nuser=sum(users),meanuser=mean(users),maxuser=max(users),minuser=min(users))
     smrdelsid=summarise(sidpredel,nsid=n(),nuser=sum(users),meanuser=mean(users),maxuser=max(users),minuser=min(users))
@@ -291,14 +236,41 @@ for(monthnow in monthlist){
                       length(unique(sidnewadd$sid)),sum(smraddsid$meanuser), length(unique(uidnewadd$liveuid)),
                       length(unique(sidpredel$sid)),sum(smrdelsid$meanuser), length(unique(uidpredel$liveuid)))
     
+    smrtotsid=ungroup(smrtotsid)
+    write.xlsx(x=top_n(arrange(smrtotsid,desc(meanuser)),100,meanuser),
+               file=paste(monthnow,"yy data.xlsx"),sheetName = paste(tname,"SidTop"),append = T,showNA = F)
 
+    #breaks=breaks=c(0,10000,50000,100000,10000000)
+    smrtotsid$dist=cut(smrtotsid$meanuser,breaks=c(0,10000,50000,100000,10000000),right=F)
+    head(smrtotsid)
+    
+    smrtotsid=group_by(smrtotsid,dist)
+    distsid=summarise(smrtotsid, nsid=n(),sumuser=sum(meanuser),muser=mean(meanuser,na.rm = T))
+    write.xlsx(x=distsid,file=paste(monthnow,"yy data.xlsx"),sheetName = paste(tname,"SidDistribution"),append = T,showNA = F)
+    
+
+
+    uidnewtot=group_by(filter(mdnew,typename==tname),liveuid)
+    uidnewadd=group_by(filter(mdnew,typename==tname & uidnew==1),liveuid)
+    uidpredel=group_by(filter(mdpre,typename==tname & uiddel==1),liveuid)
+    #length(group_size(gsidnew))
     
     smrtotuid=summarise(uidnewtot,nuid=n(),nuser=sum(users),meanuser=mean(users),maxuser=max(users),minuser=min(users))
     smradduid=summarise(uidnewadd,nuid=n(),nuser=sum(users),meanuser=mean(users),maxuser=max(users),minuser=min(users))
     smrdeluid=summarise(uidpredel,nuid=n(),nuser=sum(users),meanuser=mean(users),maxuser=max(users),minuser=min(users))
     
-    if (length(unique(uidnewadd$liveuid))>0) write.csv(file=paste(monthnow,tname,"newadd liveuid top100.csv",sep="-"),
-                                                       top_n(arrange(smradduid,desc(meanuser)),100,meanuser))
+    
+    smrtotuid=ungroup(smrtotuid)
+    write.xlsx(x=top_n(arrange(smrtotuid,desc(meanuser)),100,meanuser),
+               file=paste(monthnow,"yy data.xlsx"),sheetName = paste(tname,"UidTop"),append = T,showNA = F)
+    
+    smrtotuid$dist=cut(smrtotuid$meanuser,breaks=c(0,10000,50000,100000,10000000),right=F)
+    head(smrtotuid)
+    
+    smrtotuid=group_by(smrtotuid,dist)
+    distuid=summarise(smrtotuid, nsid=n(),sumuser=sum(meanuser),muser=mean(meanuser,na.rm = T))
+    write.xlsx(x=distuid,file=paste(monthnow,"yy data.xlsx"),sheetName = paste(tname,"UidDistribution"),append = T,showNA = F)
+    
     
   }
   
@@ -309,25 +281,27 @@ for(monthnow in monthlist){
 }
 
 
-resid.table=resid.list[[1]]
-reuid.table=resid.list[[1]]
-for (i in (2:length(resid.list))) {
-  if (length(resid.list)<2) break
-  resid.table=rbind(resid.table,resid.list[[i]])
-  reuid.table=rbind(reuid.table,reuid.list[[i]])
+if (F){
+  resid.table=resid.list[[1]]
+  reuid.table=resid.list[[1]]
+  for (i in (2:length(resid.list))) {
+    if (length(resid.list)<2) break
+    resid.table=rbind(resid.table,resid.list[[i]])
+    reuid.table=rbind(reuid.table,reuid.list[[i]])
+  }
+  
+  resbytype.table=resbytype.list[[1]]
+  for (i in (2:length(resbytype.list))) {
+    if (length(resbytype.list)<2) break
+    resbytype.table=rbind(resbytype.table,resbytype.list[[i]])
+  }
+  write.csv(file=paste("sid user number summary.csv"),resid.table)
+  write.csv(file=paste("uid user number summary.csv"),reuid.table)
+  write.csv(file=paste("user number summary by type.csv"),resbytype.table)
+  
+  save(file="./resbytype.Rdata",resid.list,reuid.list,resbytype.list)
+  
 }
-
-resbytype.table=resbytype.list[[1]]
-for (i in (2:length(resbytype.list))) {
-  if (length(resbytype.list)<2) break
-  resbytype.table=rbind(resbytype.table,resbytype.list[[i]])
-}
-write.csv(file=paste("sid user number summary.csv"),resid.table)
-write.csv(file=paste("uid user number summary.csv"),reuid.table)
-write.csv(file=paste("user number summary by type.csv"),resbytype.table)
-
-save(file="./resbytype.Rdata",resid.list,reuid.list,resbytype.list)
-
 
 
 
@@ -378,9 +352,14 @@ names(dayres.list)=c("uniquesid","uniqueuid","meanuser")
 dayres.list
 save(file="dayres.Rdata",dayres.list)
 
-write.csv(file=paste("uniquesid daily summary.csv"),dayres.list[[1]])
-write.csv(file=paste("uniqueuid daily summary.csv"),dayres.list[[2]])
-write.csv(file=paste("meanuser daily summary.csv"),dayres.list[[3]])
+write.xlsx(x=dayres.list[[1]],file=paste(max(monthlist),"yy daily by type.xlsx"),sheetName ="uniquesid",append = T,showNA = F)
+write.xlsx(x=dayres.list[[2]],file=paste(max(monthlist),"yy daily by type.xlsx"),sheetName ="uniqueuid",append = T,showNA = F)
+write.xlsx(x=dayres.list[[3]],file=paste(max(monthlist),"yy daily by type.xlsx"),sheetName ="meanuser",append = T,showNA = F)
+
+
+#write.csv(file=paste("uniquesid daily summary.csv"),dayres.list[[1]])
+#write.csv(file=paste("uniqueuid daily summary.csv"),dayres.list[[2]])
+#write.csv(file=paste("meanuser daily summary.csv"),dayres.list[[3]])
 
 
 
@@ -393,8 +372,8 @@ write.csv(file=paste("meanuser daily summary.csv"),dayres.list[[3]])
 
 
 
-test=tbl_df(select(filter(yy,mon %in% c(201505)),date,typename)) #previous
-length(which(test$typename=="entertain"))
+#test=tbl_df(select(filter(yy,mon %in% c(201505)),date,typename)) #previous
+#length(which(test$typename=="entertain"))
 
 
 
@@ -454,9 +433,10 @@ for (n in 1:length(seqtime)) {
   
 }
   
-write.xlsx(file=paste(max(monthlist),"yy_mean_result_byhour.xlsx"),meanuser)
-write.xlsx(file=paste(max(monthlist),"yy_uid_result_byhour.xlsx"),uniqueuid)
-write.xlsx(file=paste(max(monthlist),"yy_sid_result_byhour.xlsx"),uniquesid)
+
+write.xlsx(file=paste(max(monthlist),"yy by type by hour.xlsx"),x=uniquesid,sheetName ="uniquesid",append = T,showNA = F)
+write.xlsx(file=paste(max(monthlist),"yy by type by hour.xlsx"),x=uniqueuid,sheetName ="uniqueuid",append = T,showNA = F)
+write.xlsx(file=paste(max(monthlist),"yy by type by hour.xlsx"),x=meanuser,sheetName ="meanuser",append = T,showNA = F)
 
 
 
@@ -468,9 +448,4 @@ write.xlsx(file=paste(max(monthlist),"yy_sid_result_byhour.xlsx"),uniquesid)
 #----------------------------------------------------------------------------------------------
 #5# gather the reuslt
 
-
-
-
-
-save(file="./yy.Rdata",yy)
 
